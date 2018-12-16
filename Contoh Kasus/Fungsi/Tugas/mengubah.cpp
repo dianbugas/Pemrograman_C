702bffcf4a7df9f90a3a654097d48a44202b5895
dianynf <dianbugas@gmail.com>
2018-12-16 17:00:30 +0700

    tugas funsi

---
+++ Contoh Kasus/Fungsi/Tugas/mengubah.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string day;
+    string hari[] = {"minggu","senin","selasa","rabu","kamis","jum'at","sabtu"};
+    cout<<"masukan hari ini, maka akan menampilkan hari kemain :  ";
+    cin>>day;
+    if(day==hari[8]){
+        cout<<hari[6];
+    }
+    for(int i=1; i<7; i++){
+        if(day==hari[i]){
+            cout<<hari[i-1];
+        }
+    }
+
+}
