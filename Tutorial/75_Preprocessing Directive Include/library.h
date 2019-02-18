a3e57b8282cef56503485d72a0c2a9c7282fef35
dianynf <dianbugas@gmail.com>
2019-02-18 11:44:43 +0700

    Tutorial Pukis

---
+++ Tutorial/75_Preprocessing Directive Include/library.h
@@ -0,0 +1,8 @@
+#include <string>
+
+#define PI 3.14159265359
+#define KUADRAT(X) (X*X)
+#define MAX(A,B) ((A > B) ? A:B)
+#define MIN(A,B) ((A < B) ? A:B)
+
+std::string data = "STRING";
---
+++ Tutorial/75_Preprocessing Directive Include/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "library.h"
+#include <string>
+
+#define PI 3.14159265359
+
+using namespace std;
+
+int main(){
+	cout << "berhasil" << endl;
+
+	cout << "PI : " << PI << endl;
+	cout << "KUADRAT: " << KUADRAT(5) << endl;
+	cout << "MAX: " << MAX(10,2) << endl;
+	cout << "MIN: " << MIN(20,1) <<endl;
+	cout << "data string: " << data << endl;
+	return 0;
+}
